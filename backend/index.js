fa5dc50189f2d9aff43990578a26a2efda213820
kowshikk007 <kowshikk786@gmail.com>
2025-03-16 18:56:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 // ✅ Enable CORS (Allow frontend to access backend)
 app.use(cors({
     origin: "https://movies-front-8nty.onrender.com"
-    methods: "GET,POST,PUT,DELETE",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
 
